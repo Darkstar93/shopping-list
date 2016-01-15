2739f25bc732637c0eb802d7a75f83c0781c4c11
Darkstar93 <thomasturek93@gmail.com>
2016-01-14 21:07:35 -0700

    background image

--- js/jquery.js
+++ js/jquery.js
@@ -1,8 +1,42 @@
-$(document).ready(function() {
- $('button').click(function() {
-
-    var new_task = $('#input').val();
-    $('#list').append('<li>'+new_task+'</li>');
-     return false;
- });
+$(document).ready(function (){
+$('body').css('opacity', '0').fadeTo(1500, 1,'swing'); 
 });
+
+$('button.submit').click(function(){
+
+	var new_task = $('input').val();
+	var $deleteButton = $('<button />').addClass('remove').text('Delete');
+	var $li = $('<li />').text(new_task);
+	var $quantityButton = $('<button />').addClass('quantity').text('Qty');
+
+
+	$('#list').append($li);
+	$quantityButton.appendTo($li);
+	$deleteButton.appendTo($li);
+
+
+
+	return false;
+});
+
+$('#list').on('click', '.remove', function() {
+		$(this).parent().remove()
+
+		console.log($(this))
+
+
+});
+
+$('#list').on('change', 'input[type=checkbox]', function(){
+	var input= $(this).parent().find('input[type=text]');
+	if ($(this).is(':checked')){
+		$(input).css('textDecoration', 'line-through');
+	}else{
+		$(input).css('textDecoration','none');
+	return false;
+
+}
+
+
+});
+	
