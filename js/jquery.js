b7705918e49210d8b95d80b56cf00b00c6c2054e
Darkstar93 <thomasturek93@gmail.com>
2016-01-20 12:20:34 -0700

    tidy code

--- js/jquery.js
+++ js/jquery.js
@@ -1,56 +1,27 @@
 $(document).ready(function() {
-	$('body').css('opacity', '0').fadeTo(1500, 1,'swing'); 
-
-
-
-	
+    $('body').css('opacity', '0').fadeTo(1500, 1, 'swing');
 });
-
 $('button.submit').click(function() {
-	var new_task = $('input').val() + "          " + $('input + input').val();
-	var $deleteButton = $('<button />').addClass('remove').text('');
-	var $li = $('<li />').text(new_task);
-	var $quantityButton = $('<input type="checkbox" />');
-
-
-
-	$('#list').append($li);
-	$quantityButton.appendTo($li);
-	$deleteButton.appendTo($li);
-	$("form").trigger("reset");
-
-	$('form').ready(function(){
-   if($(this).val() == ''){
-      alert('Enter list items');
-   }
+    var new_task = $('input').val() + " x" + $('input + input').val();
+    var $deleteButton = $('<button />').addClass('remove').text('');
+    var $li = $('<li />').text(new_task);
+    var $quantityButton = $('<input type="checkbox" />');
+    $('#list').append($li);
+    $quantityButton.appendTo($li);
+    $deleteButton.appendTo($li);
+    $("form").trigger("reset");
+    return false;
 });
-
-
-	return false;
-});
-
-
-
 $('#list').on('click', '.remove', function() {
-	$(this).parent().remove();
-	console.log($(this));
-
-
+    $(this).parent().remove();
+    console.log($(this));
 });
-
-
-
-
 $('#list').on('change', 'input[type="checkbox"]', function() {
-	var $li = $(this).parent();
-
-	if ($(this).is(':checked')){
-		$li.css('textDecoration', 'line-through');
-	} else {
-		$li.css('textDecoration', 'none');
-		return false;
-	}
-
-
-
+    var $li = $(this).parent();
+    if ($(this).is(':checked')) {
+        $li.css('textDecoration', 'line-through');
+    } else {
+        $li.css('textDecoration', 'none');
+        return false;
+    }
 });
